34294b27a91719d5c06431a6df71cb6de15ab250
Tuke <75539193+TukeCS@users.noreply.github.com>
2023-11-19 19:25:16 +0100

    Update gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -9,14 +9,19 @@ void gui() {
 
     ImGui::Begin("AutoSens", &oppened, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
 
-    ImGui::Text("This is some useful text.");
 
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    //everything that is in the frame goes inside of the brackets
+    {
+        ImGui::Text("This is some useful text.");
 
-    if (ImGui::Button("Button"))
-        counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
+        if (ImGui::Button("Button"))
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+    }
+
 
     ImGui::End();
 }
