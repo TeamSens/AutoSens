fb9ee5d80f0194ed5396c6247291cc99e15dca64
Tuke <75539193+TukeCS@users.noreply.github.com>
2023-11-20 10:53:34 +0100

    spaced out etc etc...

--- gui.cpp
+++ gui.cpp
@@ -12,16 +12,20 @@ void interface::gui() {
     {
         const float width = ImGui::GetWindowWidth();
         const float combo_width = width * 0.25f;
+
         SetNextItemWidth(combo_width);
+
         Combo("Select Current Game", &currentGame, games, numberOfGames);
+
         SetNextItemWidth(combo_width);
+
         Combo("Select Game to Change Sensitivity", &newGame, games, numberOfGames);
         
         if (currentGame == newGame) {
             Text("Games Cannot be the Same!");
         }
         else if (Button("Config My Sens!")) {
-            
+            // run the conf script
         }
     }
 
